b81b0b04ca804ecd82c4ffacf7e563224b2e68a6
sindhu-2k19 <vsindhu.2k18@gmail.com>
2019-09-21 01:36:26 -0400

    "test3"

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,14 +1,9 @@
 pipelineJob('Pipeline2') {
   description("Pipeline created automatically through Task3")
   displayName('Project D')
- scm {git {
-         remote {
-                name('origin')
-                url('https://github.com/vsindhu-2k19/myautomationjenkins2.git')
-               }
-           }
+  scm {
+        github('vsindhu-2k19/myautomationjenkins2') 
       }  
-   
 definition {
          cps  {
                 script(readFileFromWorkspace('C2.groovy'))
