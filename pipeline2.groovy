383d0bb86e99197d4dc4db7e20fadbb4a47587f3
sindhu-2k19 <vsindhu.2k18@gmail.com>
2019-09-21 02:39:01 -0400

    "test"

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,16 +1,8 @@
 pipelineJob('Pipeline2') {
 
-checkout([$class: 'GitSCM', 
-branches: [[name: '*/master']], 
-browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/vsindhu-2k19/myautomationjenkins2.git'], 
-doGenerateSubmoduleConfigurations: false, 
-extensions: [], 
-submoduleCfg: [], 
-userRemoteConfigs: [[url: 'https://github.com/vsindhu-2k19/myautomationjenkins2.git']]])
-
   definition {
         cps {
-            script(readFileFromWorkspace('project-a-workflow.groovy'))
+            script(readFileFromWorkspace('C2.groovy'))
             sandbox()
         }
       }
